7554dbb83723cbbeab39f4790093a812f349112e
Filipe David Manana <fdmanana@apache.org>
2010-12-28 10:57:04 +0000

    Added file:allocate/2
    This function allows preallocation of space for files.
    It succeeds only on systems (OS/filesystem) that implement this feature.
    After a successful call, the actual file size might remain unchanged. This
    behaviour depends on the actual file space preallocation implementation.
    To guarantee the file size is updated, if desired, one needs to truncate
    the file to the new length.
    
    On GNU/Linux this function calls the Linux specific system call fallocate.
    On Mac OS X it calls the fcntl F_PREALLOCATE operation.
    On other Unix systems it calls the system call posix_fallocate if available.
    On systems that do not implement this feature the error {error, enosys} is
    returned.

--- lib/kernel/test/prim_file_SUITE.erl
+++ lib/kernel/test/prim_file_SUITE.erl
@@ -50,6 +50,8 @@
 
 -export([advise/1]).
 
+-export([allocate/1]).
+
 -include_lib("test_server/include/test_server.hrl").
 -include_lib("kernel/include/file.hrl").
 
@@ -80,7 +82,7 @@ groups() ->
        cur_dir_1a, cur_dir_1b]},
      {files, [],
       [{group, open}, {group, pos}, {group, file_info},
-       truncate, sync, datasync, advise]},
+       truncate, sync, datasync, advise, allocate]},
      {open, [],
       [open1, modes, close, access, read_write, pread_write,
        append, exclusive]},
@@ -1204,6 +1206,45 @@ advise(Config) when is_list(Config) ->
     ?line {ok, Line2} = ?PRIM_FILE:read_line(Fd9),
     ?line eof = ?PRIM_FILE:read_line(Fd9),
     ?line ok = ?PRIM_FILE:close(Fd9),
+
+    ?line test_server:timetrap_cancel(Dog),
+    ok.
+
+allocate(suite) -> [];
+allocate(doc) -> "Tests that ?PRIM_FILE:allocate/2 at least doesn't crash.";
+allocate(Config) when is_list(Config) ->
+    ?line Dog = test_server:timetrap(test_server:seconds(5)),
+    ?line PrivDir = ?config(priv_dir, Config),
+    ?line Allocate = filename:join(PrivDir,
+			       atom_to_list(?MODULE)
+			       ++"_allocate.fil"),
+
+    Line1 = "Hello\n",
+    Line2 = "World!\n",
+
+    ?line {ok, Fd} = ?PRIM_FILE:open(Allocate, [write]),
+    ?line ok = ?PRIM_FILE:allocate(Fd, iolist_size([Line1, Line2])),
+    ?line ok = ?PRIM_FILE:write(Fd, Line1),
+    ?line ok = ?PRIM_FILE:write(Fd, Line2),
+    ?line ok = ?PRIM_FILE:close(Fd),
+
+    ?line {ok, Fd2} = ?PRIM_FILE:open(Allocate, [write]),
+    ?line ok = ?PRIM_FILE:allocate(Fd2, iolist_size(Line1)),
+    ?line ok = ?PRIM_FILE:write(Fd2, Line1),
+    ?line ok = ?PRIM_FILE:write(Fd2, Line2),
+    ?line ok = ?PRIM_FILE:close(Fd2),
+
+    ?line {ok, Fd3} = ?PRIM_FILE:open(Allocate, [write]),
+    ?line ok = ?PRIM_FILE:allocate(Fd3, iolist_size(Line1) + 1),
+    ?line ok = ?PRIM_FILE:write(Fd3, Line1),
+    ?line ok = ?PRIM_FILE:write(Fd3, Line2),
+    ?line ok = ?PRIM_FILE:close(Fd3),
+
+    ?line {ok, Fd4} = ?PRIM_FILE:open(Allocate, [write]),
+    ?line ok = ?PRIM_FILE:allocate(Fd4, 4 * iolist_size([Line1, Line2])),
+    ?line ok = ?PRIM_FILE:write(Fd4, Line1),
+    ?line ok = ?PRIM_FILE:write(Fd4, Line2),
+    ?line ok = ?PRIM_FILE:close(Fd4),
 
     ?line test_server:timetrap_cancel(Dog),
     ok.
